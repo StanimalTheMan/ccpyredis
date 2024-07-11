3d0a3d52bd260d8ec008ac80ca74fd86e0b8cd16
Stan Choi <sjc591@nyu.edu>
2024-07-11 10:01:30 -0400

    Add virtual env and set up some resp protocol handler using tdd

---
+++ tests/test_protocol.py
@@ -0,0 +1,21 @@
+import pytest
+
+from pyredis.protocol import SimpleString, extract_frame_from_buffer
+
+def test_read_frame_simple_string_incomplete_frame():
+    buffer = b"+Par"
+    frame, frame_size = extract_frame_from_buffer(buffer)
+    assert frame == None
+    assert frame_size == 0
+
+def test_read_frame_simple_string_complete_frame():
+    buffer = b"+OK\r\n"
+    frame, frame_size = extract_frame_from_buffer(buffer)
+    assert frame == SimpleString("OK")
+    assert frame_size == 5
+
+def test_read_frame_simple_string_extra_data():
+    buffer = b"+OK\r\n+Next"
+    frame, frame_size = extract_frame_from_buffer(buffer)
+    assert frame == SimpleString("OK")
+    assert frame_size == 5
